d13583fd206eea283fd695f267e059d46fe35dd7
Will Moreland <will@moreland.nz>
2020-04-12 19:09:34 +1200

    Add script for building binaries

---
+++ build-binaries.sh
@@ -0,0 +1,19 @@
+VERSION="1.0.0"
+
+declare -a TARGET_RIDS=(
+    "win10-x64"
+    "linux-arm"
+    "linux-x64"
+)
+
+for RID in "${TARGET_RIDS[@]}"
+do
+    dotnet publish -r $RID -c Release //p:PublishSingleFile=true //p:PublishTrimmed=true -o ./bin
+    if [[ $RID == win* ]]
+    then
+        mv ./bin/NetworkUptimeMonitor.exe ./bin/NetworkUptimeMonitor-$VERSION-$RID.exe
+    else
+        mv ./bin/NetworkUptimeMonitor ./bin/NetworkUptimeMonitor-$VERSION-$RID
+    fi
+    rm ./bin/*.pdb
+done
