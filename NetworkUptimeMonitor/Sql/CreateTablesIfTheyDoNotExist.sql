5f8d950cbf78511c8b558cf7b9452e265afbc705
Will Verland <will@ver.land>
2023-11-04 16:23:18 +1300

    Add appropriate DB indexes

--- NetworkUptimeMonitor/Sql/CreateTablesIfTheyDoNotExist.sql
+++ NetworkUptimeMonitor/Sql/CreateTablesIfTheyDoNotExist.sql
@@ -1,11 +1,10 @@
-BEGIN;
-
 CREATE TABLE IF NOT EXISTS uptime_results
 (
 	uptime_result_id	INTEGER PRIMARY KEY,
 	date_time_utc		TEXT	NOT NULL,
 	was_up				INTEGER	NOT NULL
 );
+CREATE INDEX IF NOT EXISTS idx_uptime_results_was_up_date_time_utc ON uptime_results(was_up, date_time_utc);
 
 CREATE TABLE IF NOT EXISTS ping_results
 (
@@ -17,5 +16,4 @@ CREATE TABLE IF NOT EXISTS ping_results
 	round_trip_time		INTEGER	NOT NULL,
 	FOREIGN KEY(uptime_result_id) REFERENCES uptime_results(uptime_result_id)
 );
-
-END;
+CREATE INDEX IF NOT EXISTS idx_ping_results_uptime_result_id ON ping_results(uptime_result_id);
